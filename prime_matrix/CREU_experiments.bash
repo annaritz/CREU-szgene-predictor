0cd01d7ae26afa13e113c0a55b27ab5aafcddb00
Alexander King <alexanderking@Alexanders-MacBook-Pro-2.local>
2018-06-08 17:21:28 -0700

    added experimental bash file

---
+++ prime_matrix/CREU_experiments.bash
@@ -0,0 +1,47 @@
+set -e  ## this will mean that the script continues
+
+
+
+
+
+echo AUCs
+python3 predict2.py -d infiles/SZ_positives.txt -o outfiles/SZ_1-layer_Full_Pos_0.100 --auc -l 1 --force -s 50
+python3 predict2.py -d infiles/SZ_positives_no_iso.txt -o outfiles/SZ_1-layer_no_iso_0.100 --auc -l 1 --force -s 50
+
+python3 predict2.py -d infiles/SZ_positives.txt -o outfiles/SZ_2-layer_0.100 --auc -l 2 --force -s 50
+python3 predict2.py -d infiles/SZ_positives.txt -o outfiles/SZ_3-layer_0.100 --auc -l 3 --force -s 50
+
+python3 predict2.py -d infiles/SZ_positives.txt -o outfiles/SZ_1-layer_0.1-sinksource_0.100 --auc -l 1 --force -s 50 --sinksource_method -c 0.1
+python3 predict2.py -d infiles/SZ_positives.txt -o outfiles/SZ_1-layer_1-sinksource_0.100 --auc -l 1 --force -s 50 --sinksource_method -c 1
+python3 predict2.py -d infiles/SZ_positives.txt -o outfiles/SZ_1-layer_5-sinksource_0.100 --auc -l 1 --force -s 50 --sinksource_method -c 5
+python3 predict2.py -d infiles/SZ_positives.txt -o outfiles/SZ_1-layer_10-sinksource_0.100 --auc -l 1 --force -s 50 --sinksource_method -c 10
+python3 predict2.py -d infiles/SZ_positives.txt -o outfiles/SZ_1-layer_20-sinksource_0.100 --auc -l 1 --force -s 50 --sinksource_method -c 20
+
+python3 predict2.py -d infiles/SZ_positives.txt -o outfiles/SZ_2-layer_0.1-sinksource_0.100 --auc -l 2 --force -s 50 --sinksource_method -c 0.1
+python3 predict2.py -d infiles/SZ_positives.txt -o outfiles/SZ_2-layer_1-sinksource_0.100 --auc -l 2 --force -s 50 --sinksource_method -c 1
+python3 predict2.py -d infiles/SZ_positives.txt -o outfiles/SZ_2-layer_5-sinksource_0.100 --auc -l 2 --force -s 50 --sinksource_method -c 5
+python3 predict2.py -d infiles/SZ_positives.txt -o outfiles/SZ_2-layer_10-sinksource_0.100 --auc -l 2 --force -s 50 --sinksource_method -c 10
+python3 predict2.py -d infiles/SZ_positives.txt -o outfiles/SZ_2-layer_20-sinksource_0.100 --auc -l 2 --force -s 50 --sinksource_method -c 20
+
+python3 predict2.py -d infiles/SZ_positives.txt -o outfiles/SZ_2-layer_0.1-sinksource_no_neg_0.100 --auc -l 2 --force -s 50 --sinksource_method -c 0.1 -w
+python3 predict2.py -d infiles/SZ_positives.txt -o outfiles/SZ_2-layer_1-sinksource_no_neg_0.100 --auc -l 2 --force -s 50 --sinksource_method -c 1 -w
+python3 predict2.py -d infiles/SZ_positives.txt -o outfiles/SZ_2-layer_5-sinksource_no_neg_0.100 --auc -l 2 --force -s 50 --sinksource_method -c 5 -w
+python3 predict2.py -d infiles/SZ_positives.txt -o outfiles/SZ_2-layer_10-sinksource_no_neg_0.100 --auc -l 2 --force -s 50 --sinksource_method -c 10 -w
+python3 predict2.py -d infiles/SZ_positives.txt -o outfiles/SZ_2-layer_20-sinksource_no_neg_0.100 --auc -l 2 --force -s 50 --sinksource_method -c 20 -w
+
+python3 predict2.py -d infiles/SZ_positives.txt -o outfiles/SZ_3-layer_0.1-sinksource_0.100 --auc -l 3 --force -s 50 --sinksource_method -c 0.1
+python3 predict2.py -d infiles/SZ_positives.txt -o outfiles/SZ_3-layer_1-sinksource_0.100 --auc -l 3 --force -s 50 --sinksource_method -c 1
+python3 predict2.py -d infiles/SZ_positives.txt -o outfiles/SZ_3-layer_5-sinksource_0.100 --auc -l 3 --force -s 50 --sinksource_method -c 5
+python3 predict2.py -d infiles/SZ_positives.txt -o outfiles/SZ_3-layer_10-sinksource_0.100 --auc -l 3 --force -s 50 --sinksource_method -c 10
+python3 predict2.py -d infiles/SZ_positives.txt -o outfiles/SZ_3-layer_20-sinksource_0.100 --auc -l 3 --force -s 50 --sinksource_method -c 20
+
+python3 predict2.py -d infiles/SZ_positives.txt -o outfiles/SZ_3-layer_0.1-sinksource_no_neg_0.100 --auc -l 3 --force -s 50 --sinksource_method -c 0.1 -w
+python3 predict2.py -d infiles/SZ_positives.txt -o outfiles/SZ_3-layer_1-sinksource_no_neg_0.100 --auc -l 3 --force -s 50 --sinksource_method -c 1 -w
+python3 predict2.py -d infiles/SZ_positives.txt -o outfiles/SZ_3-layer_5-sinksource_no_neg_0.100 --auc -l 3 --force -s 50 --sinksource_method -c 5 -w
+python3 predict2.py -d infiles/SZ_positives.txt -o outfiles/SZ_3-layer_10-sinksource_no_neg_0.100 --auc -l 3 --force -s 50 --sinksource_method -c 10 -w
+python3 predict2.py -d infiles/SZ_positives.txt -o outfiles/SZ_3-layer_20-sinksource_no_neg_0.100 --auc -l 3 --force -s 50 --sinksource_method -c 20 -w
+
+
+
+
+echo DONE.
