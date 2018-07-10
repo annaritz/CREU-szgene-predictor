bf46ca3257457f95b1e72da5965eaa8e974b316b
Alexander King <alexanderking@Alexanders-MacBook-Pro-2.local>
2018-07-10 10:24:18 -0700

    Changed the negatives for autism as well. There seems to be slightly better performance

--- source/code/run_experiments.bash
+++ source/code/run_experiments.bash
@@ -4,7 +4,6 @@ set -e  ## this will mean that the script continues
 echo AUC RUNS
 
 
-python3 predict.py   -o ../outfiles/SZ_2-layer_1-sinksource --auc -l 2 --force -s 50 --sinksource_method -c 1
-
+python3 predict.py   -o ../outfiles/SZ_4-layer_0.15-sinksource --auc -l 4 --force -s 50 --sinksource_method -c 0.15 
 
 echo DONE.
