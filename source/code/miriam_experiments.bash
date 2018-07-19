747abb8a9ca1841fe47eb0ee00d1cf97dff8f812
Alexander King <alexanderking@Alexanders-MacBook-Pro-2.local>
2018-07-19 16:36:48 -0700

    fixed miriams experiement set

--- source/code/miriam_experiments.bash
+++ source/code/miriam_experiments.bash
@@ -36,31 +36,32 @@ python3 predict.py   -o ../outfiles/ASD_1-layer_50-sinksource --auc -l 1 --force
 
 echo FIGURE 6
 
-python3 predict.py   -o ../outfiles/SZ_3-layer_0-sinksource --auc -l 3 --force  --sinksource_method -c 0
+python3 predict.py   -o ../outfiles/SZ_2-layer_0-sinksource --auc -l 2 --force  --sinksource_method -c 0
 
-python3 predict.py   -o ../outfiles/SZ_3-layer_0.01-sinksource --auc -l 3 --force  --sinksource_method -c 0.01
+python3 predict.py   -o ../outfiles/SZ_2-layer_0.01-sinksource --auc -l 2 --force  --sinksource_method -c 0.01
 
-python3 predict.py   -o ../outfiles/SZ_3-layer_0.1-sinksource --auc -l 3 --force  --sinksource_method -c 0.1
+python3 predict.py   -o ../outfiles/SZ_2-layer_0.1-sinksource --auc -l 2 --force  --sinksource_method -c 0.1
 
-python3 predict.py   -o ../outfiles/SZ_3-layer_1-sinksource --auc -l 3 --force  --sinksource_method -c 1
+python3 predict.py   -o ../outfiles/SZ_2-layer_1-sinksource --auc -l 2 --force  --sinksource_method -c 1
 
-python3 predict.py   -o ../outfiles/SZ_3-layer_10-sinksource --auc -l 3 --force  --sinksource_method -c 10
+python3 predict.py   -o ../outfiles/SZ_2-layer_10-sinksource --auc -l 2 --force  --sinksource_method -c 10
 
-python3 predict.py   -o ../outfiles/SZ_3-layer_50-sinksource --auc -l 3 --force  --sinksource_method -c 50
+python3 predict.py   -o ../outfiles/SZ_2-layer_50-sinksource --auc -l 2 --force  --sinksource_method -c 50
 
 
 
-python3 predict.py   -o ../outfiles/ASD_3-layer_0-sinksource --auc -l 3 --force  --sinksource_method -c 0 -d ../infiles/ASD_positives.txt -n ../infiles/ASD_negatives.txt
+python3 predict.py   -o ../outfiles/ASD_2-layer_0-sinksource --auc -l 2 --force  --sinksource_method -c 0 -d ../infiles/ASD_positives.txt -n ../infiles/ASD_negatives.txt
 
-python3 predict.py   -o ../outfiles/ASD_3-layer_0.01-sinksource --auc -l 3 --force  --sinksource_method -c 0.01 -d ../infiles/ASD_positives.txt -n ../infiles/ASD_negatives.txt
+python3 predict.py   -o ../outfiles/ASD_2-layer_0.01-sinksource --auc -l 2 --force  --sinksource_method -c 0.01 -d ../infiles/ASD_positives.txt -n ../infiles/ASD_negatives.txt
 
-python3 predict.py   -o ../outfiles/ASD_3-layer_0.1-sinksource --auc -l 3 --force  --sinksource_method -c 0.1 -d ../infiles/ASD_positives.txt -n ../infiles/ASD_negatives.txt
+python3 predict.py   -o ../outfiles/ASD_2-layer_0.1-sinksource --auc -l 2 --force  --sinksource_method -c 0.1 -d ../infiles/ASD_positives.txt -n ../infiles/ASD_negatives.txt
 
-python3 predict.py   -o ../outfiles/ASD_3-layer_1-sinksource --auc -l 3 --force  --sinksource_method -c 1 -d ../infiles/ASD_positives.txt -n ../infiles/ASD_negatives.txt
+python3 predict.py   -o ../outfiles/ASD_2-layer_1-sinksource --auc -l 2 --force  --sinksource_method -c 1 -d ../infiles/ASD_positives.txt -n ../infiles/ASD_negatives.txt
 
-python3 predict.py   -o ../outfiles/ASD_3-layer_10-sinksource --auc -l 3 --force  --sinksource_method -c 10 -d ../infiles/ASD_positives.txt -n ../infiles/ASD_negatives.txt
+python3 predict.py   -o ../outfiles/ASD_2-layer_10-sinksource --auc -l 2 --force  --sinksource_method -c 10 -d ../infiles/ASD_positives.txt -n ../infiles/ASD_negatives.txt
 
-python3 predict.py   -o ../outfiles/ASD_3-layer_50-sinksource --auc -l 3 --force  --sinksource_method -c 50 -d ../infiles/ASD_positives.txt -n ../infiles/ASD_negatives.txt
+python3 predict.py   -o ../outfiles/ASD_2-layer_50-sinksource --auc -l 2 --force  --sinksource_method -c 50 -d ../infiles/ASD_positives.txt -n ../infiles/ASD_negatives.txt
+
 
 
 echo DONE.
