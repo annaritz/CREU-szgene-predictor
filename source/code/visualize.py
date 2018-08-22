e4023c39633bb4c456149a2285fb2f7b989248b0
Anna Ritz <aritz@reed.edu>
2018-08-21 20:20:02 -0700

    done with paper

--- source/code/visualize.py
+++ source/code/visualize.py
@@ -6,7 +6,10 @@ from graphspace_python.api.client import GraphSpace
 from graphspace_python.graphs.classes.gsgraph import GSGraph 
 import matplotlib.pyplot as plt
 #graphspace = GraphSpace('mirbern@reed.edu', 'pumpkin')
-graphspace = GraphSpace('aritz@reed.edu','platypus')
+#graphspace = GraphSpace('aritz@reed.edu','platypus')
+username = 'aritz@reed.edu'
+password='platypus'
+group='CREU'
 
 
 def parse_arguments(argv):
@@ -17,7 +20,7 @@ def parse_arguments(argv):
     #Input Files
     group = OptionGroup(parser,'Input Files')
     group.add_option('-n', '--network',\
-        type='string',metavar='STR',default='../networkfiles/brain_top_geq_0.150.txt',\
+        type='string',metavar='STR',default='../infiles/networkfiles/brain_top_geq_0.150.txt',\
         help='Functional interaction network (default 0.150 threshold)')
     
     #Method Arguments
@@ -35,6 +38,11 @@ def parse_arguments(argv):
     group.add_option('-s', '--SZ_positive',\
         action='store_true',default=False,\
         help='Indicate if candidate is a SZ positive (default=False')
+
+    group = OptionGroup(parser, 'Method Argument.')
+    group.add_option('-a', '--all_neighbors',\
+        action='store_true',default=False,\
+        help='Plot all neighbors instead of labeled neighbors (default=False)')
 
     #parse command line arguments
     (opts, args) = parser.parse_args()
@@ -57,7 +65,7 @@ def main(argv):
 
         print('Visualizing local graph...')
 
-        visualize_graph(graphspace, opts.gene, opts.motility_positive, opts.SZ_positive, opts.network[-9:-4], cand_neighbors, neg_neighbors, SZ_pos_neighbors, CM_pos_neighbors, genemap)
+        visualize_graph(opts.all_neighbors,opts.gene, opts.motility_positive, opts.SZ_positive, opts.network[-9:-4], cand_neighbors, neg_neighbors, SZ_pos_neighbors, CM_pos_neighbors, genemap)
 
         print('Done!')
 
@@ -111,7 +119,7 @@ def geneMapReader(infile):
     return genemap
 
 
-def visualize_graph(graphspace, candidate, CM_positive, SZ_positive, threshold, neighbors, adj_neg, adj_SZ_pos, adj_CM_pos, genemap):
+def visualize_graph(all_neighbors,candidate, CM_positive, SZ_positive, threshold, neighbors, adj_neg, adj_SZ_pos, adj_CM_pos, genemap):
     #Red if negative, blue if CM positive, yellow if SZ positive, green if double positive, orange if unlabeled
     #TODO: make a color dictionary
 
@@ -121,13 +129,17 @@ def visualize_graph(graphspace, candidate, CM_positive, SZ_positive, threshold, 
     neg_pos = adj_neg.intersection(adj_SZ_pos).union(adj_neg.intersection(adj_CM_pos)) #get set of nodes that are both positives and negatives
     print('Neg and pos:', neg_pos)
 
-    local_graph = GSGraph()
-    time = str(datetime.now())
-    local_graph.set_name('Local Graph of %s in %s threshold network %s' % (candidate, threshold, time))
-    local_graph.set_tags(['CREU'])
-
     #Create the candidate node + node style
     symbol = genemap.get(candidate)
+
+    local_graph = GSGraph()
+    time = str(datetime.now())
+    if all_neighbors:
+        local_graph.set_name('All Neighbors %s (%s)' % (symbol,candidate))
+    else:
+        local_graph.set_name('Labeled Neighbors %s (%s)' % (symbol,candidate))
+    local_graph.set_tags(['CREU'])
+
     
     if CM_positive:
         popup = '%s, %s, %s' % (candidate, 'Candidate', 'Cell Motility positive')
@@ -143,6 +155,18 @@ def visualize_graph(graphspace, candidate, CM_positive, SZ_positive, threshold, 
         local_graph.add_node_style(candidate, color='#affef2', height=40, width=100)
 
     #Create the neighbor nodes + styles
+
+    ## Add legend nodes
+    local_graph.add_node('leg1', popup='SZ & CM Pos', label='SZ & CM Positive')
+    local_graph.add_node_style('leg1', color='#5de83e', height=30, width=150)
+    local_graph.add_node('leg2', popup='Unlabeled', label='Unlabeled')
+    local_graph.add_node_style('leg2', color='#affef2', height=30, width=90)
+    local_graph.add_node('leg3', popup='Negative', label='Negative')
+    local_graph.add_node_style('leg3', color='#e56b52', height=30, width=90)     
+    local_graph.add_node('leg4', popup='CM Positive', label='CM Positive')
+    local_graph.add_node_style('leg4', color='#6699ff', height=30, width=100)
+    local_graph.add_node('leg5', popup='SZ Positive', label='SZ Positive')
+    local_graph.add_node_style('leg5', color='#fffc66', height=30, width=100)  
 
     for neighbor in neighbors:
 
@@ -176,25 +200,44 @@ def visualize_graph(graphspace, candidate, CM_positive, SZ_positive, threshold, 
             local_graph.add_node(neighbor, popup=popup, label=symbol)
             local_graph.add_node_style(neighbor, color='#fffc66', height=30, width=90)  
 
-        else:
-            #local_graph.add_node(neighbor, popup=symbol, label=symbol)
-            #local_graph.add_node_style(neighbor, color='#affef2', height=30, width=90)
-            #for now, don't show unlabeled neighbors
-            continue
-            
-            
+        elif all_neighbors:
+            local_graph.add_node(neighbor, popup=symbol, label='')
+            local_graph.add_node_style(neighbor, color='#affef2', height=20, width=20)
 
         
         local_graph.add_edge(candidate,neighbor)
         local_graph.add_edge_style(candidate,neighbor, width=neighbors[neighbor]*50)
 
-    graphspace.post_graph(local_graph)
-    print('Graph posted')
+    gid = post(local_graph,username,password,group)
+    print('Graph posted',gid)
     
 
     return
 
 
+
+def post(G,username,password,group=None):
+  '''
+  Post a graph to graphspace.  If this graph is new, share it with
+  the group.
+  Inputs: Graph (GSGraph Object)
+  Outputs: the graph ID (int)
+  '''
+  print('Posting graph...')
+  
+  # connect to GraphSpace with the username and password.
+  gs = GraphSpace(username,password)
+  try:
+    # try updating the graph. If the graph does not exist, 
+    # this will throw an error.
+    graph = gs.update_graph(G)
+  except:
+    # catch the error and try posting a new graph.
+    graph = gs.post_graph(G)
+    if group:
+        # share this graph with the group.
+        gs.share_graph(graph_id=graph.id,group_name=group)
+  return graph.id
 
 
 
